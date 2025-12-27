e087955ff9bff04ecaaf331241418d3ba31bd5a4
Ryan McKeown <ryanmckeown@mail4me.com>
2025-12-27 09:48:30 +0000

    Add tests to d9 p2

--- day9/puzzle2.py
+++ day9/puzzle2.py
@@ -1,99 +1,112 @@
+from __future__ import annotations
+
 import itertools
+from collections import Counter
 from collections.abc import Iterator, Sequence
 from functools import cache
+from typing import Optional
+
+# x-axis: Left to right = less to more
+# y-axis: Top to bottom = less to more
 
 
 class Rectangle:
     def __init__(self, p1: tuple[int, int], p2: tuple[int, int]) -> None:
-        self.bottom_y = max(p1[1], p2[1])
-        self.top_y = min(p1[1], p2[1])
-        self.left_x = min(p1[0], p2[0])
-        self.right_x = max(p1[0], p2[0])
+        self.bottom = max(p1[1], p2[1])
+        self.top = min(p1[1], p2[1])
+        self.left = min(p1[0], p2[0])
+        self.right = max(p1[0], p2[0])
 
     @property
     def area(self) -> int:
-        width = self.right_x - self.left_x + 1
-        height = self.bottom_y - self.top_y + 1
+        width = self.right - self.left + 1
+        height = self.bottom - self.top + 1
         area = width * height
         return area
 
     def iter_corners(self) -> Iterator[tuple[int, int]]:
-        yield (self.left_x, self.top_y)
-        yield (self.right_x, self.top_y)
-        yield (self.right_x, self.bottom_y)
-        yield (self.left_x, self.bottom_y)
+        yield (self.left, self.top)
+        yield (self.right, self.top)
+        yield (self.right, self.bottom)
+        yield (self.left, self.bottom)
 
     def iter_edges(self) -> Iterator[tuple[int, int]]:
-        for x in range(self.left_x, self.right_x):
-            yield (x, self.top_y)
+        for x in range(self.left, self.right):
+            yield (x, self.top)
 
-        for y in range(self.top_y, self.bottom_y):
-            yield (self.right_x, y)
+        for y in range(self.top, self.bottom):
+            yield (self.right, y)
 
-        for x in range(self.right_x, self.left_x, -1):
-            yield (x, self.bottom_y)
+        for x in range(self.right, self.left, -1):
+            yield (x, self.bottom)
 
-        for y in range(self.bottom_y, self.top_y, -1):
-            yield (self.left_x, y)
+        for y in range(self.bottom, self.top, -1):
+            yield (self.left, y)
 
     def iter_points(self) -> Iterator[tuple[int, int]]:
-        for y in range(self.top_y, self.bottom_y + 1):
-            for x in range(self.left_x, self.right_x + 1):
+        for y in range(self.top, self.bottom + 1):
+            for x in range(self.left, self.right + 1):
                 yield (x, y)
 
 
 class Line:
     def __init__(self, p1: tuple[int, int], p2: tuple[int, int]) -> None:
-        self.horizontal = p1[1] == p2[1]
-        self.vertical = p1[0] == p2[0]
-        if self.horizontal and p1[0] < p2[0]:
-            self.p1 = p1
-            self.p2 = p2
-        elif self.horizontal:
-            self.p1 = p2
-            self.p2 = p1
-        elif self.vertical and p1[1] < p2[1]:
-            self.p1 = p1
-            self.p2 = p2
-        else:
-            self.p1 = p2
-            self.p2 = p1
+        self.p1 = p1
+        self.p2 = p2
 
-    def extended_point_crosses(self, point: tuple[int, int]) -> bool:
+        self.bottom = max(p1[1], p2[1])
+        self.top = min(p1[1], p2[1])
+        self.left = min(p1[0], p2[0])
+        self.right = max(p1[0], p2[0])
+
+        self.horizontal = self.top == self.bottom
+        self.vertical = self.left == self.right
+        if self.horizontal is False and self.vertical is False:
+            raise NotImplementedError("Only horizontal or vertical lines allowed")
+
+    def extended_point_crosses_at(
+        self, point: tuple[int, int]
+    ) -> Optional[tuple[int, int]]:
         if self.horizontal:
             # The line is left-to-right, and points on the boundary count as inside
-            return False
+            return None
 
-        if point[1] <= self.p1[1] or point[1] >= self.p2[1]:
-            # The point misses the ends of the line
-            return False
-
-        if point[0] >= self.p1[0]:
+        if point[0] >= self.right:
             # The point is already to the right of the line
-            return False
+            return None
 
-        return True
+        if point[1] < self.top or point[1] >= self.bottom:
+            # The point misses the ends of the line
+            return None
+
+        return (self.left, point[1])
 
     def __iter__(self):
-        self.current = self.p1
-        self.end = self.p2
+        if self.horizontal:
+            self.current = (self.left, self.top)
+            self.end = (self.right, self.top)
+            return self
+
+        self.current = (self.left, self.top)
+        self.end = (self.left, self.bottom)
         return self
 
     def __next__(self) -> tuple[int, int]:
-        if self.horizontal:
-            n = (self.current[0] + 1, self.current[1])
-            if n[0] > self.end[0]:
-                raise StopIteration
-
-            self.current = n
-            return n
-
-        n = (self.current[0], self.current[1] + 1)
-        if n[1] > self.end[1]:
+        if self.horizontal and self.current[0] > self.end[0]:
             raise StopIteration
 
-        self.current = n
-        return n
+        if self.vertical and self.current[1] > self.end[1]:
+            raise StopIteration
+
+        this = self.current
+
+        if self.horizontal:
+            self.current = (self.current[0] + 1, self.current[1])
+
+        if self.vertical:
+            self.current = (self.current[0], self.current[1] + 1)
+
+        return this
 
 
 class Polygon:
@@ -102,37 +115,20 @@ class Polygon:
         self.lines = tuple(
             Line(points[i], points[i + 1]) for i in range(len(points) - 1)
         )
+        self.boundary = set(point for line in self.lines for point in line)
 
     @cache
     def is_inside(self, point: tuple[int, int]) -> bool:
-        count_crosses = sum(
-            int(line.extended_point_crosses(point)) for line in self.lines
+        if point in self.boundary:
+            return True
+
+        crossing_points = tuple(
+            line.extended_point_crosses_at(point) for line in self.lines
         )
-        return bool(count_crosses % 2)
-
-
-class Plot:
-    def __init__(self) -> None:
-        self._marks: dict[tuple[int, int], str] = {}
-
-    def mark(self, x: int, y: int, marker: str) -> None:
-        self._marks[(x, y)] = marker
-
-    def get(self, x: int, y: int) -> str:
-        return self._marks.get((x, y), " ")
-
-    def print(self) -> None:
-        left = min(p[0] for p, m in self._marks.items() if m == "0")
-        right = max(p[0] for p, m in self._marks.items() if m == "0")
-        top = min(p[1] for p, m in self._marks.items() if m == "0")
-        bottom = max(p[1] for p, m in self._marks.items() if m == "0")
-        with open("day9/solution.txt", "w") as f:
-            for y in range(top, bottom + 1):
-                marks_this_line = {p: m for p, m in self._marks.items() if p[1] == y}
-                line = "".join(
-                    marks_this_line.get((x, y), " ") for x in range(left, right + 1)
-                )
-                f.write(line + "\n")
+        non_overlapping_crossings = tuple(
+            p for p, n in Counter(crossing_points).items() if n == 1 and p is not None
+        )
+        return bool(len(non_overlapping_crossings) % 2)
 
 
 def data_to_points(data: str) -> list[tuple[int, int]]:
@@ -147,7 +143,11 @@ def data_to_points(data: str) -> list[tuple[int, int]]:
 def all_rectangles_biggest_to_smallest(
     points: list[tuple[int, int]],
 ) -> list[Rectangle]:
-    rects = [Rectangle(p1, p2) for p1, p2 in itertools.combinations(points, 2)]
+    rects = [
+        Rectangle(p1, p2)
+        for p1, p2 in itertools.permutations(points, 2)
+        if p1[0] <= p2[0] and p1[1] <= p2[1] and (p1[0] < p2[0] or p1[1] < p2[1])
+    ]
     rects.sort(key=lambda x: x.area, reverse=True)
     return rects
 
@@ -158,26 +158,23 @@ def solve(data: str) -> int:
     rectangles = all_rectangles_biggest_to_smallest(points)
     for rect in rectangles:
         rect_ok = True
-        for p in rect.iter_corners():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
+        if rect_ok:
+            for p in rect.iter_corners():
+                if poly.is_inside(p) is False:
+                    rect_ok = False
+                    break
 
-        if rect_ok is False:
-            continue
+        if rect_ok:
+            for p in rect.iter_edges():
+                if poly.is_inside(p) is False:
+                    rect_ok = False
+                    break
 
-        for p in rect.iter_edges():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
-
-        if rect_ok is False:
-            continue
-
-        for p in rect.iter_points():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
+        if rect_ok:
+            for p in rect.iter_points():
+                if poly.is_inside(p) is False:
+                    rect_ok = False
+                    break
 
         if rect_ok:
             return rect.area
@@ -185,71 +182,8 @@ def solve(data: str) -> int:
     return 0
 
 
-def plot_solution(data: str) -> None:
-    plot = Plot()
-
-    points = data_to_points(data)
-    for p in points:
-        plot.mark(p[0], p[1], "•")
-
-    poly = Polygon(points)
-    for line in poly.lines:
-        for p in line:
-            if plot.get(p[0], p[1]) != "•":
-                if line.horizontal:
-                    plot.mark(p[0], p[1], "-")
-                else:
-                    plot.mark(p[0], p[1], "|")
-
-    rectangles = all_rectangles_biggest_to_smallest(points)
-    for rect in rectangles:
-        rect_ok = True
-        for p in rect.iter_corners():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
-
-        if rect_ok is False:
-            continue
-
-        for p in rect.iter_edges():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
-
-        if rect_ok is False:
-            continue
-
-        for p in rect.iter_points():
-            if poly.is_inside(p) is False:
-                rect_ok = False
-                break
-
-        if rect_ok is False:
-            continue
-
-        print(rect.area)
-
-        for p in rect.iter_points():
-            if plot.get(p[0], p[1]) in (" ", "•"):
-                plot.mark(p[0], p[1], "0")
-
-        plot.print()
-        break
-
-
 if __name__ == "__main__":
-    test_data = """7,1
-11,1
-11,7
-9,7
-9,5
-2,5
-2,3
-7,3"""
-    plot_solution(test_data)
-
     with open("day9/input.txt") as f:
         points_data = f.read()
 
-    plot_solution(points_data)
+    print(solve(points_data))
